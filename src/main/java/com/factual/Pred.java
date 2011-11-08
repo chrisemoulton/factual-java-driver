7b5a89868ec8c115ba326e7169d545f6de50f03c
Aaron <aaron@aaron-desktop.(none)>
2011-11-08 11:18:54 -0800

    first working version of row filtering with Pred class

---
+++ src/main/java/com/factual/Pred.java
@@ -0,0 +1,81 @@
+package com.factual;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+
+import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
+
+/**
+ * Represents a logical predicate to be used when filtering. Supports
+ * arbitrarily complex nesting with other Preds.
+ * <p>
+ * Knows how to represent itself as JSON, ready to be sent used as filters in
+ * Factual's public API.
+ * 
+ * @author aaron
+ */
+public class Pred {
+  private final String op;
+  private final Object[] args;
+  private final ObjectMapper objectMapper = new ObjectMapper();
+
+
+  public Pred(String op, Object... args) {
+    this.op = op;
+    this.args = args;
+  }
+
+  /**
+   * @return the JSON representation of this Pred
+   */
+  public String toJsonStr() {
+    if(isSpecialForm()) {
+      return "{\"" + op + "\":" + toString(args) + "}";
+    } else {
+      Object[] rest = Arrays.copyOfRange(args, 1, args.length);
+      return "{\"" + args[0] + "\":{\"" + op + "\":" + toString(rest) + "}}";
+    }
+  }
+
+  private boolean isSpecialForm() {
+    return "$and".equals(op) || "$or".equals(op);
+  }
+
+  private String toString(Object... objs) {
+    if(objs.length == 1) {
+      return toJsonStrReflect(objs[0]);
+    } else {
+      List<String> elems = Lists.newArrayList();
+      for(Object obj : objs) {
+        elems.add(toJsonStrReflect(obj));
+      }
+      return "[" + Joiner.on(",").join(elems) + "]";
+    }
+  }
+
+  private String toJsonStrReflect(Object obj) {
+    if (obj instanceof Pred) {
+      return ((Pred)obj).toJsonStr();
+    } else {
+      return toJsonStr(obj);
+    }
+  }
+
+  private String toJsonStr(Object obj) {
+    try {
+      return objectMapper.writeValueAsString(obj);
+    } catch (JsonGenerationException e) {
+      throw new RuntimeException(e);
+    } catch (JsonMappingException e) {
+      throw new RuntimeException(e);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
