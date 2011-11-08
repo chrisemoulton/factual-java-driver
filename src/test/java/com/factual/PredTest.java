6f541dedb67fca410d43af49dde566a77fe31432
Aaron <aaron@aaron-desktop.(none)>
2011-11-08 11:19:36 -0800

    initial tests for Query and Pred

---
+++ src/test/java/com/factual/PredTest.java
@@ -0,0 +1,33 @@
+package com.factual;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+
+public class PredTest {
+
+  /**
+   * Tests a top-level AND with a nested OR and an $in:
+   * 
+   * {$and:[
+   *   {region:{$in:["MA","VT","NH"]}},
+   *   {$or:[
+   *     {first_name:{$eq:"Chun"}},
+   *     {last_name:{$eq:"Kok"}}]}]}
+   */
+  @Test
+  public void testComplicated() {
+    Pred in = new Pred("$in", "region", "MA", "VT", "NH");
+
+    Pred fname = new Pred("$eq", "first_name", "Chun");
+    Pred lname = new Pred("$eq", "last_name", "Kok");
+    Pred or = new Pred("$or", fname, lname);
+
+    Pred and = new Pred("$and", in, or);
+    String predJsonStr = and.toJsonStr();
+    assertEquals("{\"$and\":[{\"region\":{\"$in\":[\"MA\",\"VT\",\"NH\"]}},{\"$or\":[{\"first_name\":{\"$eq\":\"Chun\"}},{\"last_name\":{\"$eq\":\"Kok\"}}]}]}",
+        predJsonStr);
+  }
+
+}
---
+++ src/test/java/com/factual/QueryTest.java
@@ -0,0 +1,44 @@
+package com.factual;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+import org.junit.Test;
+
+
+/**
+ * Unit tests for Query.
+ * 
+ * @author aaron
+ */
+public class QueryTest {
+
+  @Test
+  public void testFilter_shorthand_eq_1() throws UnsupportedEncodingException {
+    Query query = new Query()
+    .filter("first_name", "Bradley");
+
+    String queryStr = query.toUrlPairs();
+    String decoded = URLDecoder.decode(queryStr, "UTF-8");
+
+    assertEquals("filters={\"$and\":{\"first_name\":{\"$eq\":\"Bradley\"}}}",
+        decoded);
+  }
+
+  @Test
+  public void testFilter_shorthand_eq_3() throws UnsupportedEncodingException {
+    Query query = new Query()
+    .filter("first_name", "Bradley")
+    .filter("region", "CA")
+    .filter("locality", "Los Angeles");
+
+    String queryStr = query.toUrlPairs();
+    String decoded = URLDecoder.decode(queryStr, "UTF-8");
+
+    assertEquals("filters={\"$and\":[{\"first_name\":{\"$eq\":\"Bradley\"}},{\"region\":{\"$eq\":\"CA\"}},{\"locality\":{\"$eq\":\"Los Angeles\"}}]}",
+        decoded);
+  }
+
+}
